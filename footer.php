9bb379464704eb0458e700acac62dda11344f97f
AJ Zane <androiddreams@ajzane.com>
2015-03-17 17:58:38 -0700

    setup header, footer, functions, menu

---
+++ footer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage WCSD 2015 Theme
+ */
+?>
+
+    </div>
+    <!-- /.container -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+<script type='text/javascript' id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.2.4.js'><\/script>".replace("HOST", location.hostname));
+//]]></script>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Twenty Fourteen functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ *
+ * @link http://codex.wordpress.org/Theme_Development
+ * @link http://codex.wordpress.org/Child_Themes
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * @link http://codex.wordpress.org/Plugin_API
+ *
+ * @package WordPress
+ * @subpackage WCSD 2015 Theme
+ */
+
+
+function wcsd_setup() { 
+  // This theme uses wp_nav_menu() in two locations.
+  register_nav_menus( array(
+    'primary'   => __( 'Top primary menu', 'wcsd' ),
+  ) );
+}
+add_action( 'after_setup_theme', 'wcsd_setup' );
