837765f1fad108a6e6cb6734b8bf10d1c2aa2c46
AJ Zane <androiddreams@ajzane.com>
2015-03-23 11:26:37 -0700

    add wp_footer()

--- footer.php
+++ footer.php
@@ -34,6 +34,8 @@
     })
     </script>
 
+<?php wp_footer(); ?>
+
 <script type='text/javascript' id="__bs_script__">//<![CDATA[
     document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.2.4.js'><\/script>".replace("HOST", location.hostname));
 //]]></script>
