5f7bf9e169e5c9ca23cab3a82721a572bd27f355
AJ Zane <androiddreams@ajzane.com>
2015-03-23 11:48:17 -0700

    rename page template so index.php will load instead and add sample menu

--- header.php
+++ header.php
@@ -63,12 +63,12 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <?php 
+                echo "<ul class='nav navbar-nav'><li>MENU</li></ul>";
                 $args = array(
                     'theme_location' => 'primary',
                     'menu_class'      => 'nav navbar-nav',
                 );
-
-                wp_nav_menu( $args );
+                // wp_nav_menu( $args );
                 ?>
             </div>
             <!-- /.navbar-collapse -->
